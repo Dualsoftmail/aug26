067a3125d2cdc92d126734f285b29e122de7d1b6
Vasyl Bentsal <Dualsoftmail@gmail.com>
2023-08-26 12:11:29 -0500

    Initial Project

---
+++ mygrade/src/main/java/com/mycompany/mygrade/Grade.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mygrade;
+
+/**
+ *
+ * @author admin
+ */
+public class Grade {
+    private String name;
+    private int math;
+    private int sci;
+    private int eng;
+
+    public Grade(String tom, int i, int i0, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMath() {
+        return math;
+    }
+
+    @Override
+    public String toString() {
+        return "Grade{" + "name=" + name + ", math=" + math + ", sci=" + sci + ", eng=" + eng + '}';
+    }
+
+    public int getSci() {
+        return sci;
+    }
+
+    public int getEng() {
+        return eng;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMath(int math) {
+        this.math = math;
+    }
+
+    public void setSci(int sci) {
+        this.sci = sci;
+    }
+
+    public void setEng(int eng) {
+        this.eng = eng;
+    }
+    public String getGrade() {
+        int total = this.math + this.eng + this.sci;
+        int avg = total / 3;
+        if (avg < 60) {
+            return "Fail";
+        } else {
+            return "Pass";
+        }
+    }
+}
